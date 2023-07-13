977edc9935f926c9698613e43bd6be3a3297b680
Erika Anglin <erikaanglin@Erikas-iMac.lan>
2023-07-13 12:06:17 -0400

    First commit

--- star_vote/static/script.js
+++ star_vote/static/script.js
@@ -1,34 +1,65 @@
-// select all elements with the 'i' tag and store them in a Nodelist called 'stars'
-const stars = document.querySelectorAll(".stars i");
+// Initial Ratings
+const ratings = {
+    rap: 4.8,
+    pop: 3.2,
+    rock: 4.6,
+    country: 1
+}
 
-// //Loop through the 'stars' NodeList
-stars.forEach((star, index1) => {
-    //add an event listener that runs a function when the 'click' event is triggered
-    star.addEventListener("click",()=> {
-        console.log(index1);
-        // //Loop through the 'stars' NodeList again
-        stars.forEach((star,index2) => {
-            //Add the "active" class to the clicked star and any stars with a lower index
-          //and remove the "active" class from any stars with a higher index  
-          index1 >= index2 ? star.classList.add("active") : star.classList.remove("active");
-        });
+//total stars
+const starsTotal=5;
 
-    });
-});    
+//run getRatings when DOM loads
+document.addEventListener('DOMContentLoaded', getRatings);
 
-const starz = document.querySelectorAll(".starz i");
+//Form elements
+const productSelect = document.getElementById('product-select');
+const ratingControl = document.getElementById('rating-control');
 
-// //Loop through the 'stars' NodeList
-starz.forEach((star, index1) => {
-    //add an event listener that runs a function when the 'click' event is triggered
-    star.addEventListener("click",()=> {
-        console.log(index1);
-        // //Loop through the 'stars' NodeList again
-        starz.forEach((star,index2) => {
-            //Add the "active" class to the clicked star and any stars with a lower index
-          //and remove the "active" class from any stars with a higher index  
-          index1 >= index2 ? star.classList.add("active") : star.classList.remove("active");
-        });
+// init product
+let product;
 
-    });
+//product select change
+productSelect.addEventListener('change', (e) => {
+    product = e.target.value;
+    //enable rating control
+    ratingControl.disabled = false;
+    ratingControl.value = ratings[product];
 });
+
+//rating control change
+ratingControl.addEventListener('blur', (e) => {
+    const rating = e.target.value;
+
+    //make sure 5 or under
+    if(rating > 5) {
+        alert('Please rate 1 - 5');
+        return;
+    }
+
+    //change rating
+    ratings[product] = rating;
+
+    getRatings();
+});
+ 
+//get ratings
+function getRatings() {
+    for (let rating in ratings) {
+        // get percentage
+        const starPercentage = (ratings[rating] /
+        starsTotal) * 100;
+
+        //round to nearest 10
+        const starPercentageRounded = `${Math.round
+            (starPercentage / 10) * 10}%`;
+
+        //set width of stars-inner to percentage
+        document.querySelector(`.${rating}
+        .stars-inner`).style.width = starPercentageRounded;
+
+        //add number rating
+        document.querySelector(`.${rating} .number-rating`).innerHTML = ratings[rating];
+    }
+}
+    
